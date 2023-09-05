51fc4c8201d935846400a6e61e78f94aa71a3924
Shamail <zahis538@newschool.edu>
2023-09-05 16:46:02 -0400

    resizing

--- shamail.js
+++ shamail.js
@@ -27,17 +27,17 @@ $(document).ready(function(){
         $(".photo").fadeIn(1000);
     });
     console.log('hewwo 2');
-    function transformScroll(event) {
-        if (!event.deltaY) {
-          return;
-        }
+    // function transformScroll(event) {
+    //     if (!event.deltaY) {
+    //       return;
+    //     }
       
-        event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
-        event.preventDefault();
-      }
+    //     event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+    //     event.preventDefault();
+    //   }
       
-      var element = document.scrollingElement || document.documentElement;
-      element.addEventListener('wheel', transformScroll);
+    //   var element = document.scrollingElement || document.documentElement;
+    //   element.addEventListener('wheel', transformScroll);
 
     // $(".interactive").click(function(){
     //     $(".interactive").hide();
